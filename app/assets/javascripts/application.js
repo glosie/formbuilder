013b287b00fc5ebafeb984d43e3ebd61208c1bf7
Greg Losie <glosie@gmail.com>
2013-07-09 16:32:02 -0700

    removed jqueryui.droppable from asset manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.draggable
-//= require jquery.ui.droppable
 //= require jquery.ui.sortable
 //= require turbolinks
 //= require lib/lodash
